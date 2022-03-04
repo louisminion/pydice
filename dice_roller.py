20f718900ffd2003e4feb03e2415f05d336213f1
louisminion <77705369+louisminion@users.noreply.github.com>
2022-03-04 01:32:50 +0000

    ASCII Art

--- dice_roller.py
+++ dice_roller.py
@@ -3,6 +3,9 @@ from numpy.random import default_rng
 rng = default_rng()
 print('Welcome to the dice roller!')
 print('###########################')
+with open('ascii-art_d20.txt') as art:
+    d20ascii = art.read()
+print(d20ascii)
 print('Enter a number of dice followed by the dice type to roll. \n For example, type 4d4 to roll 4 d4s or d20 to roll a d20')
 from time import sleep
 def overprint(text,repl, t=1, char=" "):
@@ -29,6 +32,8 @@ def parse_command(command):
     numdice = cmds[0]
     if numdice == '':
         numdice = '1'
+    if numdice == 'a':
+        numdice = 'Advantage'
     dicetype = cmds[1]
     return numdice, dicetype
 def roll_dice(dicetype):
@@ -38,7 +43,11 @@ def roll_dice(dicetype):
     poss_vales = arrs[dicetype]
     roll = rng.choice(poss_vales)
     return roll
-
+def bigger(x,y):
+    if x > y:
+        return x
+    else: 
+        return y
 
 
 while True:
@@ -50,10 +59,21 @@ while True:
     # overprint_mult(['[#    ]', '[##   ]', '[###  ]', '[#### ]', '[#####]', 'Loaded'])
     print('')
     numdice, dicetype = parse_command(command)
+    if numdice == 'Advantage':
+        result = roll_dice(dicetype)
+        result2 = roll_dice(dicetype)
+        print('Rolling d{} with advantage'.format(dicetype))
+        # print(result)
+        # print(result2)
+        overprint_mult([str(result), str(result2), str(bigger(result, result2))])
+        print('Result is {}'.format(bigger(result, result2)))
+        continue
     print('Rolling d{}'.format(dicetype))
     running_total = 0
     for i in range(0, int(numdice)):
         result = roll_dice(dicetype)
+        if result == 20 and dicetype == '20':
+            print(d20ascii)
         running_total += result
         print(result)
     print('Total: {}'.format(running_total))
